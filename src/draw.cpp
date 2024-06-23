f3060a134cbd49724edb593a182bc7e1fab41b5f
2110500-Chen Guoqiang <qq2084579048@outlook.com>
2024-06-24 01:59:14 +0800

    window styles

--- src/draw.cpp
+++ src/draw.cpp
@@ -29,32 +29,50 @@ U+257x	╰	╱	╲	╳	╴	╵	╶	╷	╸	╹	╺	╻	╼	╽	╾	╿
 
 namespace dw{
 
+    const std::string style1[7] = {" ", "─", "│", "┌", "┐", "└", "┘"};
+    const std::string style2[7] = {" ", "═", "║", "╔", "╗", "╚", "╝"};
+    const std::string style3[7] = {" ", "━", "┃", "┏", "┓", "┗", "┛"};
+    const std::string style4[7] = {" ", "─", "│", "╭", "╮", "╰", "╯"};
+    const std::string* cur_style = style4;
+
     inline int block2col(const int& b){
         return 2 * b - 1;
     }
 
     void firstline(const int& width){
+        // assert(width >= 2);
+        // std::cout << " ┌";
+        // for(int col = 0; col < width-2 ; col++){
+        //     std::cout << "──";
+        // }
+        // std::cout << "┐";
         assert(width >= 2);
-        std::cout << " ┌";
+        std::cout << cur_style[0] << cur_style[3];
         for(int col = 0; col < width-2 ; col++){
-            std::cout << "──";
+            std::cout << cur_style[1] << cur_style[1];
         }
-        std::cout << "┐";
+        std::cout << cur_style[4];
     }
     void lastline(const int& width){
+        // assert(width >= 2);
+        // std::cout << " └";
+        // for(int col = 0; col < width-2 ; col++){
+        //     std::cout << "──";
+        // }
+        // std::cout << "┘";
         assert(width >= 2);
-        std::cout << " └";
+        std::cout << cur_style[0] << cur_style[5];
         for(int col = 0; col < width-2 ; col++){
-            std::cout << "──";
+            std::cout << cur_style[1] << cur_style[1];
         }
-        std::cout << "┘";
+        std::cout << cur_style[6];
     }
     void otherline(const int&top, const int& left, const int& width, const int& height){
         for(int row = top + 1; row < top + height -1; row ++){
             tc::mov_to(row, block2col(left));
-            std::cout << " │";
+            std::cout << cur_style[0] << cur_style[2];
             tc::mov_to(row, block2col(left + width - 1));
-            std::cout << "│";
+            std::cout << cur_style[2];
         }
     }
     void window(const int &top, const int &left, const int &width, const int &height, const std::string title){
